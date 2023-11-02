186a638edb7f30e7ca78d6e623e2993e4c104995
Monty Williams <roddrickwilliams123@gmail.com>
2023-11-02 10:47:09 -0600

    add both text fields to scaffold project didnt specify if we needed to build it within the widget itself

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,53 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:holbegram/widgets/text_field.dart';
+
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    name: 'com.monty.holbegram',
+    options: FirebaseOptions(
+      apiKey: 'AIzaSyAPvrjviXgA7I86-YQtNiBDkshdEsgb8vM',
+      projectId: 'holbegram-a7aa4',
+      storageBucket: 'holbegram-a7aa4.appspot.com',
+      messagingSenderId: '722036419309',
+      appId: '1:722036419309:android:79287b183af389779e3ae4',
+    ),
+  );
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+    const MyApp({Key? Key}) : super(key: Key);
+    @override
+    Widget build(BuildContext context) {
+        return MaterialApp(
+            title: 'Wubaduba dub dub!',
+            home: Scaffold(
+                body: Center(
+                 child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                            TextFieldInput(
+                                controller: TextEditingController(),
+                                hintText: "email",
+                                suffixIcon: Icon(Icons.search),
+                                keyboardType: TextInputType.text,
+                        ),
+                        TextFieldInput(
+                                controller: TextEditingController(),
+                                hintText: "password",
+                                isPassword: true,
+                                suffixIcon: Icon(Icons.password),
+                                keyboardType: TextInputType.text,
+                        ),
+                    ],
+                ),
+             ),
+          )
+
+        );
+    }
 }
